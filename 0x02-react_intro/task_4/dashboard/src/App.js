ee504fc535fc8def91a94999b4f754b26b40820b
bullbearan <4403@holbertonstudents.com>
2023-05-10 19:00:30 +0100

    add

---
+++ 0x02-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.jpeg";
+import "./App.css";
+import { getFooterCopy, getFullYear } from "./utils.js";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="App-header">
+				<img src={logo} alt="holberton logo" />
+				<h1>School dashboard</h1>
+			</div>
+			<div className="App-body">
+				<p>Login to access the full dashboard</p>
+				<label htmlFor="email">Email: </label>
+				<input type="email" id="email" name="email"></input>
+				<label htmlFor="password">Password: </label>
+				<input type="password" id="password" name="password"></input>
+				<button>OK</button>
+			</div>
+			<div className="App-footer">
+				<p>
+					Copyright {getFullYear()} - {getFooterCopy()}
+				</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.test.js
@@ -0,0 +1,19 @@
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
+import React from "react";
+
+describe("<Notifications />", () => {
+	it("renders <Notifications /> component", () => {
+		shallow(<Notifications />);
+	});
+
+	it("check number of lists", () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.find("li")).toHaveLength(3);
+	});
+
+	it("check the paragraph", () => {
+		const wrapper = shallow(<Notifications />);
+		expect(wrapper.contains(<p>Here is the list of notifications</p>)).toEqual(true);
+	});
+});
