b3ac75d79ccfad82d4b68bdfa58fbcb71e006090
bullbearan <4403@holbertonstudents.com>
2023-06-13 11:37:49 +0100

    add

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,16 +1,13 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
-  const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
+  const capitalizetheletter = (str) => str.charAt(0).toUpperCase() + str.slice(1);
   const students = Seq(object)
     .filter((grade) => grade.score > 70)
     .map((student) => ({
       score: student.score,
-      firstName: capitalize(student.firstName),
-      lastName: capitalize(student.lastName),
+      firstName: capitalizetheletter(student.firstName),
+      lastName: capitalizetheletter(student.lastName),
     }))
     .toJS();
   
